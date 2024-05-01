c3a0ec32f46efd95cd1fefd9be0740ebdf78bfd6
Neethu-Chandran-S <147372779+Neethu-Chandran-S@users.noreply.github.com>
2024-05-01 18:23:23 +0000

    3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,request
 import pickle
 import numpy as np
 app=Flask(__name__)
-model=pickle.load(open('model (2).pkl','rb'))
+model=pickle.load(open('model.pkl','rb'))
 
 
 @app.route('/')
